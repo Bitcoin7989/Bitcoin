b5f5637f13249fce5a5e120d032e384a3692822c
Bitcoin7989 <btcminer4655@gmail.com>
2024-08-18 20:16:49 +0530

    Update updateRPM.js

--- updateRPM.js
+++ updateRPM.js
@@ -1,23 +1,6 @@
-        async function updateRPM() {
-            try {
-                const response = await fetch('/rpm');
-                if (response.ok) {
-                    const data = await response.text();
-                    const rpmElement = document.getElementById('rpm');
-                    if (rpmElement) {
-                        rpmElement.innerText = 'Current RPM: ' + data;
-                    } else {
-                        console.error('Element with ID "rpm" not found.');
-                    }
-                } else {
-                    console.error('Failed to fetch RPM. Status:', response.status);
+  setInterval(function() {
+                fetch('/rpm').then(response=>response.text()).then(data=>{
+                    document.getElementById('rpm').innerText = 'Current RPM: ' + data;
                 }
-            } catch (error) {
-                console.error('Error fetching RPM:', error);
-            }
-        }
-
-        // Ensure the DOM is fully loaded before executing the script
-        document.addEventListener('DOMContentLoaded', (event) => {
-            updateRPM();
-        });
+                );
+            }, 1000);
