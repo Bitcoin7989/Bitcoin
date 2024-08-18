ea55bffc78a5a4e1585e5fd9efd173572fb1b2fd
Bitcoin7989 <btcminer4655@gmail.com>
2024-08-18 20:42:25 +0530

    Update updateRPM.js

--- updateRPM.js
+++ updateRPM.js
@@ -1,6 +1,6 @@
   setInterval(function() {
                 fetch('/rpm').then(response=>response.text()).then(data=>{
-                    document.getElementById('rpm').innerText = 'Current RPM: ' + data;
+                    document.getElementById('rpm').innerText = '' + data;
                 }
                 );
             }, 1000);
